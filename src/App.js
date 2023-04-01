a2a64b50c86c4c01a3b8037c4ed5941d60a556ed
anirudh-tiwari <anirudht2001@gmail.com>
2023-04-01 21:27:40 +0530

    add: routes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,43 @@ function App() {
                         </button>
                     </TooltipComponent> 
                 </div>
+                {activeMenu ? (
+                    <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">SIDEBAR</div>
+                ):(<div className="w-0 dark:bg-secondary-dark-bg">SIDEBAR w-0</div>)}
+                <div className={ `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu?'md:ml-72':'flex-2'}`}>
+                    <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+                        Navbar
+                    </div>
+                </div>
+                <div>
+                <Routes>
+                {/* DASHBOARD  */}
+                <Route path="/" element="ECommerce"  />    
+                <Route path="/ecommerce" element="ECommerce"  />  
+
+                {/* PAGES   */}
+                <Route path="/orders" element="Orders" />
+                <Route path="/editor" element="Editor" />
+                <Route path="/customers" element="Customers" />
+
+                {/* Apps  */}
+                <Route path="/kanban" element="Kanban" />
+                <Route path="/editor" element="Editor" />
+                <Route path="/calender" element="Calender" />
+                <Route path="/color-picker" element="ColorPicker" />
+
+                {/* Charts   */}
+                <Route path="/line" element="Line" />
+                <Route path="/area" element="Area" />
+                <Route path="/bar" element="Bar" />
+                <Route path="/pie" element="Pie" />
+                <Route path="/financial" element="Financial" />
+                <Route path="/color-maping" element="ColorMaping" />
+                <Route path="/pyramid" element="Pyramid" />
+                <Route path="/stacked" element="Stacked" />
+
+                </Routes>    
+                </div>
             </div>
             </BrowserRouter>
         </div>
