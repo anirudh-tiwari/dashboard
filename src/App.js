9972edf948dfacf8ad6eb084b1a967a6aa08ce42
anirudh-tiwari <anirudht2001@gmail.com>
2023-04-07 22:12:09 +0530

    add: ecommerce layout

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
                     <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                         <Navbar />
                     </div>
-                </div>
+
                 <div>
                 <Routes>
                 {/* DASHBOARD  */}
@@ -57,6 +57,7 @@ function App() {
                 <Route path="/stacked" element={<Stacked /> } />
 
                 </Routes>    
+                </div>
                 </div>
             </div>
             </BrowserRouter>
